e1025b3d463427f0e35787b642fa6956cbecf609
Elijah <elijah.pyatihatko@gmail.com>
2023-02-08 23:24:40 +0200

    editorjs styling on create route

--- src/routes/create/index.tsx
+++ src/routes/create/index.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import React, { useEffect } from 'react'
+import './styles.css'
 import EditorJS from '@editorjs/editorjs';
 import Button from '../../components/Button'
 
@@ -29,27 +30,26 @@ const CreatePage = (props: Props) => {
         </div>
         <div className='flex flex-col'>
           <span className='mb-2 text-sm font-extralight text-slate-300'>Content:</span>
-          <div className='bg-slate-300' id='editorjs'/>
+          <div className='bg-slate-300 rounded' id='editorjs'/>
         </div>
-          
         <label className='flex flex-col'>
-            <span className='mb-2 text-sm font-extralight text-slate-300'>Post thumbnail:</span>
-            <input 
-              type="file" 
-              className={clsx(
-                "text-sm text-slate-300",
-                "file:mr-5 file:py-2 file:px-6",
-                "file:rounded-full file:border-0",
-                "file:text-sm file:font-medium",
-                "file:bg-blue-50 file:text-slate-700",
-                "hover:file:cursor-pointer hover:file:bg-green-50",
-                "hover:file:text-green-500"
-              )}
-           />
+          <span className='mb-2 text-sm font-extralight text-slate-300'>Post thumbnail:</span>
+          <input 
+            type="file" 
+            className={clsx(
+              "text-sm text-slate-300",
+              "file:mr-5 file:py-2 file:px-6",
+              "file:rounded-full file:border-0",
+              "file:text-sm file:font-medium",
+              "file:bg-blue-50 file:text-slate-700",
+              "hover:file:cursor-pointer hover:file:bg-green-50",
+              "hover:file:text-green-500"
+            )}
+          />
         </label>
       </div>
       <div className='fixed bottom-10 left-0 w-full grid place-items-center'>
-        <Button className='py-3 px-11 text-md'>Create post</Button>
+        <Button className='py-3 px-11 text-md shadow-md'>Create post</Button>
       </div>
     </div>
   )
