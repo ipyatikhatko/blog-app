0ff7e7695827546c9d29ff96236bb3c2b0eeb3e2
Elijah <elijah.pyatihatko@gmail.com>
2023-02-12 21:41:44 +0200

    fix full width

--- src/routes/bookmarks/index.tsx
+++ src/routes/bookmarks/index.tsx
@@ -3,9 +3,9 @@ import ArticlePreview from '../../components/ArticlePreview';
 
 const BookmarksPage = () => {
 	return (
-		<div className="relative flex w-2/3 flex-col gap-4 mobile:w-full">
+		<div className="relative flex w-full flex-col gap-4 mobile:w-full">
 			<h1 className="sticky top-5 left-5 mb-4 w-fit rounded-full bg-slate-500 px-4 text-2xl font-[700] text-green-50 shadow-md">
-				Recently added
+				Bookmarks
 			</h1>
 			<div className="flex flex-col gap-8 mobile:p-5">
 				<ArticlePreview />
